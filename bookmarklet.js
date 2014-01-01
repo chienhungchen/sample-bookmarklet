ddba6a29a7ecb2c1289e5b0c30e569283e079c38
Jeff Chen <jeff@tackk.com>
2013-12-31 23:14:24 -0600

    added $

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,3 +1,31 @@
-(function(){
-	alert('from external');
-})();
+(function(root, undefined){
+	'use strict';
+
+	console.log('External JS Bookmarklet pulled in.');
+	
+	var Bookmarklet = {
+		//Define vars in here
+	};
+	
+	Bookmarklet.main = function() {
+		console.log('bookmarklet main');
+
+	};
+
+	Bookmarklet.init = function(){
+		console.log('bookmarklet init');
+
+		if (!($ = window.jQuery)) { // typeof jQuery=='undefined' works too  
+			script = document.createElement( 'script' );  
+			script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js';   
+		    script.onload = Bookmarklet.main;  
+		    document.body.appendChild(script);
+		}   
+		else {  
+		    Bookmarklet.main();
+		}
+	};
+
+
+
+})(this);
